ee5a4ab787743c8fe48580c3bcd5ee20e3f96de4
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-19 19:19:05 +0100

    quote fix / add player_api

--- slice.sh
+++ slice.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd minetest_game
-git subtree split -P "mods/$1" -b "$1"
-git push "https://${access_token}@github.com/minetest-game/$1" "$1"
+git subtree split -P mods/$1 -b $1
+git push https://${access_token}@github.com/minetest-game/$1 $1
