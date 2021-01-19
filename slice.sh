15f50df5f2f7d92dfee5f6280748726f33b4102c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-19 20:47:59 +0100

    accesstoken test

--- slice.sh
+++ slice.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo ${access_token} | md5sum -
 cd minetest_game
 git subtree split -P mods/$1 -b $1
 git push "https://BuckarooBanzay:${access_token}@github.com/minetest-game/${1}.git" $1:master
