a10260493ed89dcf233fc39062c5406fb548dd4a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-19 19:57:43 +0100

    env

--- slice.sh
+++ slice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GITHUB_REPO_URL=https://git:${access_token}@github.com/minetest-game/${1}
+GITHUB_REPO_URL=https://git:${access_token}@github.com/minetest-game/${1}.git
 
 cd minetest_game
 git subtree split -P mods/$1 -b $1
