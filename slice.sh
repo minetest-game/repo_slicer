c559afdafde5f1cd7b613dbfe018e70332559a93
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-19 20:50:15 +0100

    debug

--- slice.sh
+++ slice.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo ${access_token} | md5sum -
+export
 cd minetest_game
-git subtree split -P mods/$1 -b $1
-git push "https://BuckarooBanzay:${access_token}@github.com/minetest-game/${1}.git" $1:master
+#git subtree split -P mods/$1 -b $1
+#git push "https://BuckarooBanzay:${access_token}@github.com/minetest-game/${1}.git" $1:master
