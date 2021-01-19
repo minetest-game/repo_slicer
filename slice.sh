51d37c6187b46f81e702e7a8f43c16b8994fac0c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-19 19:50:40 +0100

    debug

--- slice.sh
+++ slice.sh
@@ -2,5 +2,5 @@
 
 cd minetest_game
 git subtree split -P mods/$1 -b $1
-git remote add $1 https://${access_token}@github.com/minetest-game/$1
+git remote add $1 https://github.com/minetest-game/$1
 git push $1 $1:master
