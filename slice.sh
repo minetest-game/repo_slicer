91f4ed4f55777c2517e3d6d2678c77f6c0dfbf61
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-19 19:55:03 +0100

    username

--- slice.sh
+++ slice.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-GITHUB_REPO_URL=https://${access_token}@github.com/minetest-game/${1}
+GITHUB_REPO_URL=https://git:${access_token}@github.com/minetest-game/${1}
 
 cd minetest_game
 git subtree split -P mods/$1 -b $1
-git remote add $1 "${GITHUB_REPO_URL}"
-git push $1 $1:master
+git push "${GITHUB_REPO_URL}" $1:master
