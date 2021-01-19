ab11ea24bffb1cda0f85d0fd32db1de94c123d81
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-19 21:18:02 +0100

    readme / schedule

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+## Minetest game mod slicer
+
+![slice](https://github.com/minetest-game/repo_slicer/workflows/slice/badge.svg)
--- slice.sh
+++ slice.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd minetest_game
 git subtree split -P mods/$1 -b $1
-git config -l
 git push "https://x-access-token:${access_token}@github.com/minetest-game/${1}.git" ${1}:master
