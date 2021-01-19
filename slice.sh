7f34747470561b52b1a490226460fc2cb2b25bc7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-19 19:25:23 +0100

    script fix

--- slice.sh
+++ slice.sh
@@ -2,4 +2,5 @@
 
 cd minetest_game
 git subtree split -P mods/$1 -b $1
-git push https://${access_token}@github.com/minetest-game/$1 $1
+pwd
+git push https://${access_token}@github.com/minetest-game/$1 $1:master
